efd10b25a11bf23ba22a70b1d97993f747a79baf
CharmanderV123 <119460986+CharmanderV123@users.noreply.github.com>
2024-06-29 17:07:15 -0400

    Updated 2002 J1 Seven Segment Display.py
    Added print statement for number 1

--- 2002 J1 Seven Segment Display.py
+++ 2002 J1 Seven Segment Display.py
@@ -1,1 +1,14 @@
 #Problem J1 Seven Segment Display
+
+num = int(input("Enter Number "))
+
+#print("number", num)
+
+if (num == 1):
+    print("   *")
+    print("   *")
+    print("   *")
+    print("")
+    print("   *")
+    print("   *")
+    print("   *")
