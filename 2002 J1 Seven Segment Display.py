5854ce66cf018239414508586c42a5fe5446452e
CharmanderV123 <119460986+CharmanderV123@users.noreply.github.com>
2024-06-29 17:24:08 -0400

    Updated 2002 J1 Seven Segment Display.py
    Completed Code Without Optimizing It

--- 2002 J1 Seven Segment Display.py
+++ 2002 J1 Seven Segment Display.py
@@ -12,3 +12,88 @@ if (num == 1):
     print("   *")
     print("   *")
     print("   *")
+
+elif (num == 2):
+    print(" * * *")
+    print("      *")
+    print("      *")
+    print("      *")
+    print(" * * *")
+    print("*")
+    print("*")
+    print("*")    
+    print(" * * *")
+
+elif (num == 3):
+    print(" * * *")
+    print("      *")
+    print("      *")
+    print("      *")
+    print(" * * *")
+    print("      *")
+    print("      *")
+    print("      *")    
+    print(" * * *")
+
+elif (num == 4):
+    print("*     *")
+    print("*     *")
+    print("*     *")
+    print(" * * *")
+    print("      *")
+    print("      *")
+    print("      *")   
+
+elif (num == 5):
+    print(" * * *")
+    print("*")
+    print("*")
+    print("*")   
+    print(" * * *")
+    print("      *")
+    print("      *")
+    print("      *") 
+    print(" * * *")
+
+elif (num == 6):
+    print(" * * *")
+    print("*")
+    print("*")
+    print("*")   
+    print(" * * *")
+    print("*     *")
+    print("*     *")
+    print("*     *") 
+    print(" * * *")
+
+elif (num == 7):
+    print(" * * *")
+    print("      *")
+    print("      *")
+    print("      *")
+    print("")
+    print("      *")
+    print("      *")
+    print("      *") 
+
+elif (num == 8):
+    print(" * * *")
+    print("*     *")
+    print("*     *")
+    print("*     *")   
+    print(" * * *")
+    print("*     *")
+    print("*     *")
+    print("*     *") 
+    print(" * * *")
+
+elif (num == 9):
+    print(" * * *")
+    print("*     *")
+    print("*     *")
+    print("*     *") 
+    print(" * * *")
+    print("      *")
+    print("      *")
+    print("      *")   
+    print(" * * *")
